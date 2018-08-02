e5893785cd49e1a59d4cd792efd0bf776122ec64
jerauld <25723428+jerauld@users.noreply.github.com>
2018-08-01 17:39:49 -0700

    restructed for shadow animation

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,29 @@
-alert("connected!");
+document.addEventListener("DOMContentLoaded", function () {
+    var buttons = document.querySelectorAll('.button');
+    var sidebar = document.querySelector('.sidebar');
+    var sidebarShadow = document.querySelector('.sidebar-shadow');
+    var sidebarItems = document.querySelectorAll('.sidebar-item');
+    var sidebarSocialItems = document.querySelectorAll('.sidebar-social-item');
+    var body = document.body;
+
+    function toggleSidebar() {
+        buttons.forEach(function(button) {
+            button.classList.toggle("active");
+        });
+        body.classList.toggle("hidden");
+        sidebar.classList.toggle("move-to-left");
+        sidebarShadow.classList.toggle("bring-forward");
+        sidebarItems.forEach(function (sidebarItem) {
+            sidebarItem.classList.toggle("active");
+        });
+        sidebarSocialItems.forEach(function (sidebarSocialItem) {
+            sidebarSocialItem.classList.toggle("active");
+        });
+    }
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function (event) {
+            toggleSidebar();
+        });
+    });
+});
