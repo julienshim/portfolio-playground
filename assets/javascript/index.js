111030a5e6e87d623e1cc49cf42f049a4676031d
jerauld <25723428+jerauld@users.noreply.github.com>
2018-08-07 16:46:39 -0700

    moving javascript from local

---
+++ assets/javascript/index.js
@@ -0,0 +1,222 @@
+var about = {
+    name: "Jerauld Manansala",
+    jobTitle: "developer",
+    image: "",
+    location: "San Francisco Bay Area",
+    skills: [
+        {
+            name: "Documentation",
+            type: "soft",
+            level: 4
+        },
+        {
+            name: "JavaScript",
+            type: "hard",
+            level: 5
+        },
+        {
+            name: "Organization",
+            type: "soft",
+            level: 5
+        },
+        {
+            name: "Express",
+            type: "hard",
+            level: 3
+        },
+        {
+            name: "MySQL",
+            type: "hard",
+            level: 3
+        },
+        {
+            name: "Leadership",
+            type: "soft",
+            level: 4
+        },
+        {
+            name: "Firebase",
+            type: "hard",
+            level: 2
+        },
+        {
+            name: "HTML/CSS",
+            type: "hard",
+            level: 5
+        },
+        {
+            name: "Communication",
+            type: "soft",
+            level: 5
+        },
+        {
+            name: "NodeJS",
+            type: "hard",
+            level: 4
+        },
+        {
+            name: "Photoshop",
+            type: "hard",
+            level: 5
+        },       {
+            name: "React-Native",
+            type: "hard",
+            level: 2
+        },
+        {
+            name: "React",
+            type: "hard",
+            level: 3
+        },
+        {
+            name: "Collaboration",
+            type: "soft",
+            level: 5
+        },
+        {
+            name: "Redux",
+            type: "hard",
+            level: 2
+        },
+        {
+            name: "jQuery",
+            type: "hard",
+            level: 4
+        },
+        {
+            name: "Design",
+            type: "soft",
+            level: 5
+        },
+        {
+            name: "MongoDB",
+            type: "hard",
+            level: 2
+        }
+    ],
+    recentWork: [
+        {
+            title: "Odin's Ravens",
+            summary: "Web application clone of the strategic card game Odin's Ravens.",
+            image: "assets/images/projects/odins-ravens.png",
+            disciplines: ["ReactJS", "Firebase", "JavaScript", "CSS", "Bootstrap", "jQuery", "OAuth"],
+            githubRepo: "https://github.com/Laethaka/OdinsWorld",
+            deployedURL: "https://evening-coast-38386.herokuapp.com/",
+            roles: ["Front End Engineer", "QA Engineer"],
+            year: 2018
+    
+        },
+        {
+            title: "Meishi",
+            summary: "Social networking business card app for iOS and Android devices.",
+            image: "assets/images/projects/meishi.png",
+            disciplines: ["Expo", "JavaScript", "Firebase", "React-Native", "Redux", "OAuth"],
+            githubRepo: "https://github.com/cliffpham/meishi-mvp",
+            deployedURL: "https://expo.io/@cliffpham/expo-sanbox",
+            roles: ["QA Engineer", "Database Developer"],
+            year: 2018
+        },
+        {
+            title: "Jumpstart",
+            summary: "Job search web application tool.",
+            image: "assets/images/projects/jumpstart.png",
+            disciplines: ["JavaScript", "EJS", "jQuery", "Bootstrap", "HTML", "CSS", "NodeJS", "Express", "MySQL", "OAuth"],
+            githubRepo: "https://github.com/jerauld/jumpstart",
+            deployedURL: "https://obscure-beach-77511.herokuapp.com/",
+            roles: ["Database Developer", "Project Manager", "QA Engineer"],
+            year: 2018
+        }
+    ],
+    targets: {
+        introduction: document.querySelector('#typedIntroduction'),
+        recentWork: document.querySelector('#recent-work'),
+        skills: document.querySelector('#skillsBubbleDiagram'),
+        ahref: document.querySelectorAll('a[href^="#"]')
+    },
+    generateSkills: function() {
+        this.targets.skills.innerHTML = `
+        ${about.skills.map(skill => `
+            <dt class="${skill.type}-skill-${skill.level}">${skill.name}</dt>
+            <dd>${skill.level}</dd>
+        `).join('')}
+    `
+    },
+    generateRecentWork: function() {
+        this.targets.recentWork.innerHTML = `
+        ${about.recentWork.map(works => `
+            <div class="project-container">
+                <div class="project-content">
+                    <div class="project-image-container">
+                        <img src="${works.image}" class="project-image" title="${works.title}" />
+                        <div class="overlay">
+                            <div class="text">
+                                <ul>
+                                    ${works.disciplines.map(discipline => `<li>${discipline}</li>`).join('')}
+                                </ul>
+                            </div>
+                            <div class="year-overlay">
+                                <p>${works.year}</p>
+                             </div>
+                            <div class="external-links-overlay">
+                                <p><a href="${works.deployedURL}" target="_blank">Github</a> <a href="${works.deployedURL}" target="_blank">Live</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="project-title">${works.title}</p>
+                    <p class="disciplines">${works.roles.join(', ')}</p>
+                    <p class="summary">${works.summary}</p>
+                </div>
+            </div>
+        `).join('')}
+    `
+    },
+    smoothScrolling: function() {
+        this.targets.ahref.forEach(anchor => {
+            anchor.addEventListener('click', function (event) {
+                event.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+    },
+    init: function(){
+        this.generateRecentWork();
+        this.generateSkills();
+        this.smoothScrolling();
+    }
+}
+
+
+about.init();
+
+// Introduction - Note: Object Literals
+
+var introduction = `<h1 class='mb-12'>Hi, my name is <span>${about.name}</span>.</h1> <h1 class='mb-48'>I am a <span>${about.jobTitle}</span> based in the <span>${about.location}</span>.</h1><p>I love designing and building full-stack web applications.</p>`,
+    i = 0,
+    isTag,
+    text;
+
+
+function screenType() {
+    character = introduction.slice(0, i++)
+    if (character === introduction) return;
+    
+    about.targets.introduction.innerHTML = character + '<span class="caret blink" aria-hidden="true"></span>';
+
+    var currentCharacter = character.slice(-1);
+    if( currentCharacter === '<' ) isTag = true;
+    if( currentCharacter === '>' ) isTag = false;
+
+    if (isTag) return screenType();
+
+    if (currentCharacter === "." | currentCharacter === "?") {
+        setTimeout(screenType, 500);
+    } else {
+        setTimeout(screenType, 25);
+    }
+  
+    
+};
+
+screenType();
---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,152 @@
+var portfolio = {
+    projects: [
+        {
+            title: "Odin's Ravens",
+            summary: "Web application clone of the strategic card game Odin's Ravens.",
+            image: "assets/images/projects/odins-ravens.png",
+            disciplines: ["ReactJS", "Firebase", "JavaScript", "CSS", "Bootstrap", "jQuery", "OAuth"],
+            githubRepo: "https://github.com/Laethaka/OdinsWorld",
+            deployedURL: "https://evening-coast-38386.herokuapp.com/",
+            roles: ["Front-End Engineer", "QA Engineer"],
+            year: 2018
+    
+        },
+        {
+            title: "Meishi",
+            summary: "Social networking business card app for iOS and Android devices.",
+            image: "assets/images/projects/meishi.png",
+            disciplines: ["Expo", "JavaScript", "Firebase", "React-Native", "Redux", "OAuth"],
+            githubRepo: "https://github.com/cliffpham/meishi-mvp",
+            deployedURL: "https://expo.io/@cliffpham/expo-sanbox",
+            roles: ["Database Developer", "QA Engineer"],
+            year: 2018
+        },
+        {
+            title: "Jumpstart",
+            summary: "Job search web application tool.",
+            image: "assets/images/projects/jumpstart.png",
+            disciplines: ["JavaScript", "EJS", "jQuery", "Bootstrap", "HTML", "CSS", "NodeJS", "Express", "MySQL", "OAuth"],
+            githubRepo: "https://github.com/jerauld/jumpstart",
+            deployedURL: "https://obscure-beach-77511.herokuapp.com/",
+            roles: ["Database Developer", "Project Manager", "QA Engineer"],
+            year: 2018
+        },
+        {
+            title: "Marvel Me",
+            summary: "Facial recognition web application to match users to the Marvel Cinematic Universe character they most resemble.",
+            image: "assets/images/projects/marvel-me.png",
+            disciplines: ["jQuery", "HTML", "CSS", "Bootstrap", "Facial Recognition", "Firebase"],
+            githubRepo: "https://github.com/tomkim825/Project1-FaceAPI",
+            deployedURL: "https://tomkim825.github.io/Project1-FaceAPI/",
+            roles: ["Front-End Engineer", "UX/UI", "Design", "Project Manager"],
+            year: 2018
+        },
+        {
+            title: "Star Wars Clicky Game",
+            summary: "Stormtrooper themed memory game built with React.",
+            image: "assets/images/projects/star-wars.png",
+            disciplines: ["ReactJS", "CSS", "JavaScript", "JSON"],
+            githubRepo: "https://github.com/jerauld/unit-4-game",
+            deployedURL: "https://jerauld.github.io/unit-4-game/",
+            roles: ["Author"],
+            year: 2018
+        },
+        {
+            title: "Zelda Rupee Game",
+            summary: "Legend of Zelda themed 'Crystals Collector'-type game dynamically updated via jQuery.",
+            image: "assets/images/projects/zelda.png",
+            disciplines: ["HTML", "CSS", "JavaScript", "jQuery"],
+            githubRepo: "https://github.com/jerauld/unit-4-game",
+            deployedURL: "https://jerauld.github.io/unit-4-game/",
+            roles: ["Author"],
+            year: 2018
+        }
+    ],
+    targets: {
+        filter: document.querySelector('.filter'),
+        portfolio: document.querySelector('#portfolio')
+    },
+    sessionStorage: {
+        filter: sessionStorage.getItem('filter')
+    }
+}
+
+// Generate Projects - Note: Object Literals
+
+function generateProjects(filteredProjects) {
+    portfolio.targets.portfolio.innerHTML = `
+        ${filteredProjects.map(project => `
+            <div class="project-container">
+                <div class="project-content mb-12p">
+                    <div class="project-image-container">
+                        <img src="${project.image}" class="project-image" title="${project.title}" />
+                        <div class="overlay">
+                            <div class="text">
+                                <ul>
+                                    ${project.disciplines.map(discipline => `<li>${discipline}</li>`).join('')}
+                                </ul>
+                            </div>
+                            <div class="year-overlay">
+                                <p>${project.year}</p>
+                            </div>
+                            <div class="external-links-overlay">
+                                <p><a href="${project.deployedURL}" target="_blank">Github</a> <a href="${project.deployedURL}" target="_blank">Live</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="project-title">${project.title}</p>
+                    <p class="disciplines">${project.roles.join(', ')}</p>
+                    <p class="summary">${project.summary}</p>
+                </div>
+            </div>
+        `).join('')}
+    `
+};
+
+// Filter Projects - Note: Object Literals
+
+function generateFilterTags() {
+
+    var disciplines = ["All"];
+
+    portfolio.projects.forEach(function(project){
+        project.disciplines.forEach(function(discipline){
+            if(!disciplines.includes(discipline)) {
+                disciplines.push(discipline);
+            };
+        });
+    });
+
+    portfolio.targets.filter.innerHTML = `
+        ${disciplines.sort().map(discipline => `<li><a href="#" class="filter-tags" data-filter="${discipline}">${discipline}</a></li>`).join('')}
+    `
+}
+
+function filterProjects(keyword) {
+    
+    var filteredProjects = [];
+
+    portfolio.projects.forEach(function(project){
+        if(project.disciplines.includes(keyword)) {
+            filteredProjects.push(project);
+        }
+    });
+
+    generateProjects(filteredProjects);
+
+}
+
+function init() {
+    generateFilterTags();
+    if(!portfolio.sessionStorage.filter){
+        sessionStorage.setItem('filter', "All");
+        generateProjects(portfolio.projects);
+    } else if(portfolio.sessionStorage.filter === "All") { 
+        sessionStorage.setItem('filter', "All");
+        generateProjects(portfolio.projects);
+    } else {
+        filterProjects(portfolio.sessionStorage.filter);
+    };
+};
+
+init();
