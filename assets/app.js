054b32c273a99b3e2cd9b93506422eab9a512d5d
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-02-24 19:53:33 -0800

    linted

--- assets/app.js
+++ assets/app.js
@@ -1,35 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var body = document.body;
-  var buttons = document.querySelectorAll(".button");
-  var currentFilterSelected = document.querySelector("#currentFilterSelected");
-  var filterTags = document.querySelectorAll(".filter-tags");
-  var sidebar = document.querySelector(".sidebar");
-  var sidebarItems = document.querySelectorAll(".sidebar-item");
-  var sidebarShadow = document.querySelector(".sidebar-shadow");
-  var sidebarSocialItems = document.querySelectorAll(".sidebar-social-item");
-  var slider = document.querySelector("#slider");
-  var trigger = document.querySelector("#sliderTrigger");
-  var filterSVG = document.querySelector("#filterSVG");
+document.addEventListener('DOMContentLoaded', () => {
+  const { body } = document;
+  const buttons = document.querySelectorAll('.button');
+  const currentFilterSelected = document.querySelector(
+    '#currentFilterSelected'
+  );
+  const filterTags = document.querySelectorAll('.filter-tags');
+  const sidebar = document.querySelector('.sidebar');
+  const sidebarItems = document.querySelectorAll('.sidebar-item');
+  const sidebarShadow = document.querySelector('.sidebar-shadow');
+  const sidebarSocialItems = document.querySelectorAll('.sidebar-social-item');
+  const slider = document.querySelector('#slider');
+  const trigger = document.querySelector('#sliderTrigger');
+  const filterSVG = document.querySelector('#filterSVG');
 
   // Sidebar Event Listeners
 
-  function toggleSidebar() {
-    buttons.forEach(function(button) {
-      button.classList.toggle("active");
+  const toggleSidebar = () => {
+    buttons.forEach(button => {
+      button.classList.toggle('active');
     });
-    body.classList.toggle("hidden");
-    sidebar.classList.toggle("move-to-left");
-    sidebarShadow.classList.toggle("bring-forward");
-    sidebarItems.forEach(function(sidebarItem) {
-      sidebarItem.classList.toggle("active");
+    body.classList.toggle('hidden');
+    sidebar.classList.toggle('move-to-left');
+    sidebarShadow.classList.toggle('bring-forward');
+    sidebarItems.forEach(sidebarItem => {
+      sidebarItem.classList.toggle('active');
     });
-    sidebarSocialItems.forEach(function(sidebarSocialItem) {
-      sidebarSocialItem.classList.toggle("active");
+    sidebarSocialItems.forEach(sidebarSocialItem => {
+      sidebarSocialItem.classList.toggle('active');
     });
-  }
+  };
 
-  buttons.forEach(function(button) {
-    button.addEventListener("click", function(event) {
+  buttons.forEach(button => {
+    button.addEventListener('click', event => {
       toggleSidebar();
     });
   });
@@ -37,49 +39,50 @@ document.addEventListener("DOMContentLoaded", function() {
   // Portfolio Filtering Event Listeners
 
   if (slider) {
-    trigger.addEventListener("click", function() {
+    trigger.addEventListener('click', () => {
+      // eslint-disable-next-line no-console
       console.log(slider.classList);
-      slider.classList.toggle("slideup");
-      slider.classList.toggle("slidedown");
-      filterSVG.classList.toggle("rotate");
+      slider.classList.toggle('slideup');
+      slider.classList.toggle('slidedown');
+      filterSVG.classList.toggle('rotate');
     });
   }
 
   if (filterTags) {
-    filterTags.forEach(function(filterTag) {
-      filterTag.addEventListener("click", function(event) {
-        var filter = event.target.dataset.filter;
+    filterTags.forEach(filterTag => {
+      filterTag.addEventListener('click', event => {
+        const { filter } = event.target.dataset;
         event.preventDefault();
-        sessionStorage.setItem("filter", filter);
-        if (filter === "All") {
-          generateFilterTags("All");
+        sessionStorage.setItem('filter', filter);
+        if (filter === 'All') {
+          generateFilterTags('All');
           generateProjects(portfolio.projects);
-          slider.classList.toggle("slideup");
-          slider.classList.toggle("slidedown");
-          currentFilterSelected.innerHTML = "";
+          slider.classList.toggle('slideup');
+          slider.classList.toggle('slidedown');
+          currentFilterSelected.innerHTML = '';
         } else {
           generateFilterTags(filter);
           filterProjects(filter);
-          slider.classList.toggle("slideup");
-          slider.classList.toggle("slidedown");
+          slider.classList.toggle('slideup');
+          slider.classList.toggle('slidedown');
           currentFilterSelected.innerHTML = `
                     <span class="filter-id" id="filterId"><a href="#">${filter} <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z"/></svg></a></span>`;
         }
-        slider.classList.toggle("slideup");
-        slider.classList.toggle("slidedown");
+        slider.classList.toggle('slideup');
+        slider.classList.toggle('slidedown');
       });
     });
   }
 
-  if (currentFilterSelected && currentFilterSelected.innerHTML.trim() !== "") {
-    currentFilterSelected.addEventListener("click", function(event) {
+  if (currentFilterSelected && currentFilterSelected.innerHTML.trim() !== '') {
+    currentFilterSelected.addEventListener('click', event => {
       event.preventDefault();
-      sessionStorage.setItem("filter", "All");
-      currentFilterSelected.innerHTML = "";
-      generateFilterTags("All");
-      if (slider.classList.contains("slidedown")) {
-        slider.classList.toggle("slideup");
-        slider.classList.toggle("slidedown");
+      sessionStorage.setItem('filter', 'All');
+      currentFilterSelected.innerHTML = '';
+      generateFilterTags('All');
+      if (slider.classList.contains('slidedown')) {
+        slider.classList.toggle('slideup');
+        slider.classList.toggle('slidedown');
       }
       generateProjects(portfolio.projects);
     });
