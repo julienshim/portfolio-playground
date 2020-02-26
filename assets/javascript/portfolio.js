182a13a4f9ba2de9682f8b865057c97245038d44
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-02-25 22:41:12 -0800

    linted

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,25 +1,31 @@
-var portfolio = {
+const portfolio = {
   projects: [],
   targets: {
-    slider: document.querySelector("#slider"),
-    filter: document.querySelector(".filter"),
-    portfolio: document.querySelector("#portfolio"),
-    currentFilterSelected: document.querySelector("#currentFilterSelected"),
-    filterTags: document.querySelectorAll(".filter-tags")
+    slider: document.querySelector('#slider'),
+    filter: document.querySelector('.filter'),
+    portfolio: document.querySelector('#portfolio'),
+    currentFilterSelected: document.querySelector('#currentFilterSelected'),
+    filterTags: document.querySelectorAll('.filter-tags')
   },
   sessionStorage: {
-    filter: sessionStorage.getItem("filter")
+    filter: sessionStorage.getItem('filter')
   }
 };
+
+function slide() {
+  if (portfolio.targets.slider.classList.contains('slidedown')) {
+    portfolio.targets.slider.classList.toggle('slideup');
+    portfolio.targets.slider.classList.toggle('slidedown');
+  }
+}
 
 // Generate Projects - Note: Object Literals
 
 function generateProjects(filteredProjects) {
   portfolio.targets.portfolio.innerHTML = `
         ${filteredProjects
-          .map(
-            project => {
-              return `
+          .map(project => {
+            return `
               <div class="project-container" onclick="void(0)">
                   <div class="project-content mb-12p">
                       <div class="project-image-container">
@@ -34,7 +40,7 @@ function generateProjects(filteredProjects) {
                                           discipline =>
                                             `<li class="filter-tags" data-filter="${discipline}">${discipline}</li>`
                                         )
-                                        .join("")}
+                                        .join('')}
                                   </ul>
                               </div>
                               <div class="year-overlay">
@@ -44,46 +50,45 @@ function generateProjects(filteredProjects) {
                                   <p><a href="${
                                     project.githubRepo
                                   }" target="_blank">Github</a>${project.deployedURL &&
-                ` <a href="${project.deployedURL}" target="_blank">Live</a>`}</p>
+              ` <a href="${project.deployedURL}" target="_blank">Live</a>`}</p>
                               </div>
                           </div>
                       </div>
                       <p class="project-title">${project.title}</p>
-                      <p class="disciplines">${project.roles.join(", ")}</p>
+                      <p class="disciplines">${project.roles.join(', ')}</p>
                       <p class="summary">${project.summary}</p>
                   </div>
               </div>
-          `
-            }
-          )
-          .join("")}
+          `;
+          })
+          .join('')}
     `;
-  clickTags();
 }
 
 // Filter Projects - Note: Object Literals
 
-function generateFilterTags(textHighlight = "") {
-  var disciplines = [];
+const generateFilterTags = (textHighlight = '') => {
+  const disciplines = [];
 
-  portfolio.projects.forEach(function(project) {
-    project.disciplines.forEach(function(discipline) {
+  portfolio.projects.forEach(project => {
+    project.disciplines.forEach(discipline => {
       if (
         !disciplines.some(disciplineObject =>
           disciplineObject.tagName.includes(discipline)
         )
       ) {
-        var newDisciplineObject = {
+        const newDisciplineObject = {
           tagName: discipline,
           count: 1,
-          isHighlighted: discipline === textHighlight ? true : false
+          isHighlighted: discipline === textHighlight
         };
 
         disciplines.push(newDisciplineObject);
       } else {
-        disciplines.forEach(function(disciplineObject) {
+        disciplines.forEach(disciplineObject => {
           if (disciplineObject.tagName === discipline) {
-            disciplineObject.count++;
+            // eslint-disable-next-line no-param-reassign
+            disciplineObject.count += 1;
           }
         });
       }
@@ -91,7 +96,7 @@ function generateFilterTags(textHighlight = "") {
   });
 
   disciplines
-    .sort(function(a, b) {
+    .sort((a, b) => {
       if (a.tagName < b.tagName) {
         return -1;
       }
@@ -102,71 +107,28 @@ function generateFilterTags(textHighlight = "") {
     })
     .unshift(
       {
-        tagName: "All",
+        tagName: 'All',
         count: portfolio.projects.length,
-        isHighlighted: textHighlight === "All" ? true : false
+        isHighlighted: textHighlight === 'All'
       },
-      "Technologies"
+      'Technologies'
     );
 
   portfolio.targets.filter.innerHTML = `
         ${disciplines
           .map(discipline =>
-            discipline === "Technologies"
+            discipline === 'Technologies'
               ? `<li class="filter-header">${discipline}</a></li>`
               : `<li><a href="#" class="filter-tags isHighlighted-${discipline.isHighlighted}" data-filter="${discipline.tagName}">${discipline.tagName}</a> <span class="tag-count">(${discipline.count})</span></li>`
           )
-          .join("")}
+          .join('')}
     `;
-  clickTags();
-}
-
-function clickTags() {
-  document.querySelectorAll(".filter-tags").forEach(function(filterTag) {
-    filterTag.addEventListener("click", function(event) {
-      var filter = event.target.dataset.filter;
-      event.preventDefault();
-      sessionStorage.setItem("filter", filter);
-      if (filter === "All") {
-        generateFilterTags("All");
-        generateProjects(portfolio.projects);
-        slide();
-        portfolio.targets.currentFilterSelected.innerHTML = "";
-      } else {
-        generateFilterTags(filter);
-        filterProjects(filter);
-        slide();
-        portfolio.targets.currentFilterSelected.innerHTML = `
-                    <span class="filter-id" id="filterId"><a href="#">${filter} <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z"/></svg></a></span>`;
-      }
-    });
-  });
-
-  if (portfolio.targets.currentFilterSelected) {
-    portfolio.targets.currentFilterSelected.addEventListener("click", function(
-      event
-    ) {
-      event.preventDefault();
-      sessionStorage.setItem("filter", "All");
-      portfolio.targets.currentFilterSelected.innerHTML = "";
-      generateFilterTags("All");
-      slide();
-      generateProjects(portfolio.projects);
-    });
-  }
-}
-
-function slide() {
-  if (portfolio.targets.slider.classList.contains("slidedown")) {
-    portfolio.targets.slider.classList.toggle("slideup");
-    portfolio.targets.slider.classList.toggle("slidedown");
-  }
-}
+};
 
 function filterProjects(keyword) {
-  var filteredProjects = [];
+  const filteredProjects = [];
 
-  portfolio.projects.forEach(function(project) {
+  portfolio.projects.forEach(project => {
     if (project.disciplines.includes(keyword)) {
       filteredProjects.push(project);
     }
@@ -175,42 +137,84 @@ function filterProjects(keyword) {
   generateProjects(filteredProjects);
 }
 
+const clickTags = () => {
+  document.querySelectorAll('.filter-tags').forEach(filterTag => {
+    filterTag.addEventListener('click', event => {
+      const { filter } = event.target.dataset;
+      event.preventDefault();
+      sessionStorage.setItem('filter', filter);
+      if (filter === 'All') {
+        generateFilterTags('All');
+        generateProjects(portfolio.projects);
+        clickTags();
+        slide();
+        portfolio.targets.currentFilterSelected.innerHTML = '';
+      } else {
+        generateFilterTags(filter);
+        filterProjects(filter);
+        clickTags();
+        slide();
+        portfolio.targets.currentFilterSelected.innerHTML = `
+                    <span class="filter-id" id="filterId"><a href="#">${filter} <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z"/></svg></a></span>`;
+      }
+    });
+  });
+
+  if (portfolio.targets.currentFilterSelected) {
+    portfolio.targets.currentFilterSelected.addEventListener('click', event => {
+      event.preventDefault();
+      sessionStorage.setItem('filter', 'All');
+      portfolio.targets.currentFilterSelected.innerHTML = '';
+      generateFilterTags('All');
+      slide();
+      generateProjects(portfolio.projects);
+      clickTags();
+    });
+  }
+};
+
 function init() {
   if (!portfolio.sessionStorage.filter) {
-    sessionStorage.setItem("filter", "All");
-    generateFilterTags("All");
+    sessionStorage.setItem('filter', 'All');
+    generateFilterTags('All');
     generateProjects(portfolio.projects);
-    portfolio.targets.currentFilterSelected.innerHTML = "";
-  } else if (portfolio.sessionStorage.filter === "All") {
-    sessionStorage.setItem("filter", "All");
+    clickTags();
+    portfolio.targets.currentFilterSelected.innerHTML = '';
+  } else if (portfolio.sessionStorage.filter === 'All') {
+    sessionStorage.setItem('filter', 'All');
     generateFilterTags(portfolio.sessionStorage.filter);
     generateProjects(portfolio.projects);
-    portfolio.targets.currentFilterSelected.innerHTML = "";
+    clickTags();
+    portfolio.targets.currentFilterSelected.innerHTML = '';
   } else {
     generateFilterTags(portfolio.sessionStorage.filter);
     filterProjects(portfolio.sessionStorage.filter);
+    clickTags();
     portfolio.targets.currentFilterSelected.innerHTML = `
         <span class="filter-id" id="filterId"><a href="#">${portfolio.sessionStorage.filter} <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z"/></svg></a></span>`;
   }
 }
 
-var xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 xhr.open(
-  "GET",
-  "https://raw.githubusercontent.com/julienshim/Portfolio-Playground/master/data.json"
+  'GET',
+  'https://raw.githubusercontent.com/julienshim/Portfolio-Playground/master/data.json'
 );
-xhr.onload = function() {
-  if (this.status === 200) {
+xhr.onload = () => {
+  if (xhr.status === 200) {
     try {
-      const resObj = JSON.parse(this.responseText);
+      const resObj = JSON.parse(xhr.responseText);
       portfolio.projects = resObj.projects;
       init();
-      console.log("Projects Loaded.")
+      // eslint-disable-next-line no-console
+      console.log('Projects Loaded.');
     } catch (error) {
-      console.warn("There was an error in the JSON. Could not parse!");
+      // eslint-disable-next-line no-console
+      console.warn('There was an error in the JSON. Could not parse!');
     }
   } else {
-    console.warn("Did not receive 200 OK for response!");
+    // eslint-disable-next-line no-console
+    console.warn('Did not receive 200 OK for response!');
   }
 };
 xhr.send();
