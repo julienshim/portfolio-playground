426aba7c7b86adf120ed166b9a215ea2ef75d167
julienshim <25723428+julienshim@users.noreply.github.com>
2023-07-07 04:29:17 -0700

    get_data_j

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -198,7 +198,7 @@ function init() {
 const xhr = new XMLHttpRequest();
 xhr.open(
   'GET',
-  'https://raw.githubusercontent.com/julienshim/portfolio-playground/master/data.json'
+  '/data.json'
 );
 xhr.onload = () => {
   if (xhr.status === 200) {
